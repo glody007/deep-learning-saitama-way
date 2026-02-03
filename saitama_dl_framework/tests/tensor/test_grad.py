62e0d5972420914d1e18341a1f05f2152b1622b4
glody007 <glodymbutwile@gmail.com>
2026-02-03 06:22:17 +0100

    test: add unit tests for subtraction and multiplication operations in Tensor class

--- saitama_dl_framework/tests/tensor/test_grad.py
+++ saitama_dl_framework/tests/tensor/test_grad.py
@@ -13,3 +13,27 @@ def test___add__():
     assert isinstance(grad_fn.next_functions[0][0], backward.Accumulated)
     assert isinstance(grad_fn.next_functions[1][0], backward.Accumulated)
     
+def test___sub__():
+    v1 = Tensor([1, 2, 3, 4])
+    v2 = Tensor([4, 3, 2, 1])
+
+    result_vec = v1 - v2
+    grad_fn = result_vec.grad_fn
+    assert result_vec.is_leaf == False
+    assert grad_fn != None
+    assert isinstance(grad_fn, backward.SubBackward)
+    assert isinstance(grad_fn.next_functions[0][0], backward.Accumulated)
+    assert isinstance(grad_fn.next_functions[1][0], backward.Accumulated)
+    
+    
+def test___mul__():
+    v1 = Tensor([1, 2, 3, 4])
+    v2 = Tensor([4, 3, 2, 1])
+
+    result_vec = v1.multiply(v2)
+    grad_fn = result_vec.grad_fn
+    assert result_vec.is_leaf == False
+    assert grad_fn != None
+    assert isinstance(grad_fn, backward.MultiBackward)
+    assert isinstance(grad_fn.next_functions[0][0], backward.Accumulated)
+    assert isinstance(grad_fn.next_functions[1][0], backward.Accumulated)
